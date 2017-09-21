02309571e6b11bef447dd62c24d46f81d8044a51
CJ Burkey <cjburkey01@gmail.com>
2017-09-21 15:24:56 -0400

    Worked on movement

--- ProjectMinotaur/Assets/Script/Player/PlayerMove.cs
+++ ProjectMinotaur/Assets/Script/Player/PlayerMove.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 [RequireComponent(typeof(CharacterController))]
 public class PlayerMove : MonoBehaviour {
-	
+
+	public float mouseSensitivity = 1.0f;
 	public float playerWalkSpeed = 1.0f;
 	public float playerRunSpeed = 2.0f;
 	public float playerJumpHeight = 2.0f;
+	public float playerGravity = 0.2f;
 
 	private CharacterController controller;
 	private float currentSpeed;
@@ -24,14 +26,33 @@ public class PlayerMove : MonoBehaviour {
 		}
 	}
 
-	// Handle mouse looking
+	// Handle mouse looking and player movement
 	void Update() {
+		MouseMovement();
+		HandleMovement();
+	}
+
+	// Handle mouse looking
+	private void MouseMovement() {
 
 	}
 
-	// Handle movement
-	void FixedUpdate() {
-
+	// Handle pedal movement
+	private void HandleMovement() {
+		if (controller.isGrounded) {
+			moveDirection.x = Input.GetAxisRaw("Horizontal");
+			moveDirection.y = 0;
+			moveDirection.z = Input.GetAxisRaw("Vertical");
+			moveDirection.Normalize();
+			moveDirection = transform.TransformDirection(moveDirection);
+			moveDirection *= currentSpeed;
+			if (Input.GetButtonDown("Jump")) {
+				moveDirection.y = playerJumpHeight;
+			}
+		} else {
+			moveDirection.y -= playerGravity * Time.deltaTime;
+		}
+		controller.Move(moveDirection * Time.deltaTime);
 	}
 
 }
