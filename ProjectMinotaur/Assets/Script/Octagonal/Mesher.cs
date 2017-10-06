5c3ceae570bff478c6f9f0cb8107e8c036f8784f
CJ Burkey <cjburkey01@gmail.com>
2017-10-06 19:47:17 -0400

    Revert "Revert "Changed quite a bit""
    This reverts commit b395271423600e846d765e3904912c88d9e0ecb9.

---
+++ ProjectMinotaur/Assets/Script/Octagonal/Mesher.cs
@@ -0,0 +1,65 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Mesher : MonoBehaviour {
+
+	private List<Vector3> verts;
+	private List<int> tris;
+	private List<Vector2> uvs;
+
+	private MeshFilter filter;
+	private MeshRenderer renderer;
+
+	void Start() {
+		verts = new List<Vector3>();
+		tris = new List<int>();
+		uvs = new List<Vector2>();
+
+		filter = GetComponent<MeshFilter>();
+		renderer = GetComponent<MeshRenderer>();
+
+		verts.Clear();
+		tris.Clear();
+		uvs.Clear();
+		AddRectangle(Vector3.zero, Vector3.zero, false);
+		UpdateMesh();
+	}
+
+	private void AddRectangle(Vector3 start, Vector3 end, bool fourfivedeg) {
+		// Front face
+		AddTriangles(verts.Count, false);
+		verts.Add(new Vector3(1.0f, 0.0f, 0.0f));
+		verts.Add(new Vector3(0.0f, 0.0f, 0.0f));
+		verts.Add(new Vector3(0.0f, 1.0f, 0.0f));
+		verts.Add(new Vector3(1.0f, 1.0f, 0.0f));
+
+		// Back face
+		AddTriangles(verts.Count, true);
+		verts.Add(new Vector3(1.0f, 0.0f, 1.0f));
+		verts.Add(new Vector3(0.0f, 0.0f, 1.0f));
+		verts.Add(new Vector3(0.0f, 1.0f, 1.0f));
+		verts.Add(new Vector3(1.0f, 1.0f, 1.0f));
+	}
+
+	private void UpdateMesh() {
+		filter.mesh = null;
+		Mesh mesh = new Mesh();
+		mesh.vertices = verts.ToArray();
+		mesh.triangles = tris.ToArray();
+		mesh.uv = uvs.ToArray();
+		mesh.RecalculateBounds();
+		mesh.RecalculateNormals();
+		mesh.RecalculateTangents();
+		filter.mesh = mesh;
+	}
+
+	private void AddTriangles(int i, bool revTris) {
+		if (revTris) {
+			tris.AddRange(new int[] { i + 1, i, i + 2, i + 3, i + 2, i });
+		} else {
+			tris.AddRange(new int[] { i + 2, i, i + 1, i, i + 2, i + 3 });
+		}
+	}
+
+}
