5f5e22f0a7875fd6477fe67e4ff70d03e2c84448
CJ Burkey <cjburkey01@gmail.com>
2017-10-19 18:02:47 -0400

    Work more on generation

---
+++ ProjectMinotaur/Assets/__NEW__/Script/Generation/MazeChunk.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+
+public class MazeChunk : MonoBehaviour {
+	
+	// Walls: { top, left }
+	public static readonly int chunkSize = 16;
+	private static readonly int[] WALLS = new int[] { 0x10, 0x01 };
+
+	private int[,] data;
+
+	void Start() {
+		data = new int[chunkSize, chunkSize];
+	}
+
+	public void GenerateChunk() {
+
+	}
+
+	public void SetWalls(int x, int y, bool top, bool left) {
+		if (InChunk(x, y)) {
+			data[x, y] = ((top) ? WALLS[0] : 0) + ((left) ? WALLS[1] : 0);
+		}
+	}
+
+	public bool HasWall(int x, int y, int wall) {
+		if (wall < WALLS.Length && InChunk(x, y)) {
+			int wallCode = WALLS[wall];
+			return (data[x, y] & wallCode) == wallCode;
+		}
+		return false;
+	}
+
+	public bool[] GetWalls(int x, int y) {
+		if (InChunk(x, y)) {
+			return new bool[] { HasWall(x, y, 0), HasWall(x, y, 1) };
+		}
+		return new bool[] { false };
+	}
+
+	public bool InChunk(int x, int y) {
+		return x < chunkSize && y < chunkSize && x >= 0 && y >= 0;
+	}
+
+}
---
+++ ProjectMinotaur/Assets/__NEW__/Script/Generation/NewMazeGenerator.cs
@@ -0,0 +1,47 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NewMazeGenerator : MonoBehaviour {
+
+	private Dictionary<ChunkPos, MazeChunk> chunks;
+
+	public int chunksX = 4;
+	public int chunksY = 4;
+
+	public float cellSize = 10.0f;
+	public float wallSize = 2.0f;
+	public float textureSize = 1.0f;
+
+	public GameObject chunkPrefab;
+
+	void Start() {
+		chunks = new Dictionary<ChunkPos, MazeChunk>();
+	}
+
+	public void GenerateMaze() {
+		for (int x = 0; x < chunksX; x++) {
+			for (int y = 0; y < chunksX; y++) {
+				AddChunk(new ChunkPos(x, y));
+			}
+		}
+	}
+
+	private void AddChunk(ChunkPos pos) {
+		GameObject obj = Instantiate<GameObject>(chunkPrefab);
+		chunks.Add(pos, obj.GetComponent<MazeChunk>());
+	}
+
+	public void RenderMaze() {
+
+	}
+
+	public int GetSizeX() {
+		return chunksX * MazeChunk.chunkSize;
+	}
+
+	public int GetSizeY() {
+		return chunksY * MazeChunk.chunkSize;
+	}
+
+}
