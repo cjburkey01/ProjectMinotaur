6a725a4428a034d35c85c17a187188b03e04b147
CJ Burkey <cjburkey01@gmail.com>
2017-11-26 20:51:40 -0500

    Begin event listener

---
+++ ProjectMinotaur/Assets/__NEW__/Script/Core/Event/PMEvent.cs
@@ -0,0 +1,5 @@
+﻿public interface PMEvent {
+
+	string GetName();
+
+}
---
+++ ProjectMinotaur/Assets/__NEW__/Script/Core/Event/PMEventSystem.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using UnityEngine;
+
+public delegate void PMEventListener(Type eventType, PMEvent eventObj);
+
+public class PMEventSystem : MonoBehaviour {
+
+	private static PMEventSystem instance;
+
+	private Dictionary<Type, List<PMEventListener>> listeners;
+
+	void Start() {
+		instance = this;
+		listeners = new Dictionary<Type, List<PMEventListener>>();
+	}
+
+	public bool RegisterListener(Type e, PMEventListener listener) {
+		if (!e.IsSubclassOf(typeof(PMEvent))) {
+			return false;
+		}
+		List<PMEventListener> inSlot;
+		if (!listeners.TryGetValue(e, out inSlot) || inSlot == null) {
+			inSlot = new List<PMEventListener>();
+		}
+		inSlot.Add(listener);
+		listeners.Add(e, inSlot);
+		return true;
+	}
+
+	public void TriggerEvent(PMEvent e) {
+
+	}
+
+	public static PMEventSystem GetEventSystem() {
+		return instance;
+	}
+
+}
